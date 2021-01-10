95e33278f6fd10c01f627dd1e0fdb145f89df7bf
Hussain-7 <hussain2000.rizvi@gmail.com>
2021-01-10 17:21:55 +0500

    added program 2 and 3

--- program3.cpp
+++ program3.cpp
@@ -1,18 +1,14 @@
-// Dynamic Programming C++ implementation to find
-// minimum number of deletions and insertions
 #include <bits/stdc++.h>
 
 using namespace std;
 
 // Returns length of length common subsequence
 // for str1[0..m-1], str2[0..n-1]
+//Function to get the number of common characters between str1 and str2 
 int lcs(string str1, string str2, int m, int n)
 {
 	int L[m + 1][n + 1];
 	int i, j;
-
-	// Following steps build L[m+1][n+1] in bottom
-	// up fashion. Note that L[i][j] contains
 	// length of LCS of str1[0..i-1] and str2[0..j-1]
 	for (i = 0; i <= m; i++) {
 		for (j = 0; j <= n; j++) {
@@ -28,7 +24,7 @@ int lcs(string str1, string str2, int m, int n)
 	}
 
 	// L[m][n] contains length of LCS
-	// for X[0..n-1] and Y[0..m-1]
+	// for str1 and str2
 	return L[m][n];
 }
 
@@ -38,22 +34,25 @@ void printMinDelAndInsert(string str1, string str2)
 {
 	int m = str1.size();
 	int n = str2.size();
-
+	//len get the longest common
 	int len = lcs(str1, str2, m, n);
-
-	cout << "Minimum number of deletions = " << (m - len)
-		<< endl;
-
-	cout << "Minimum number of insertions = " << (n - len)
-		<< endl;
+	cout << "Minimum number of deletions = " << (m - len)<< endl;
+	cout << "Minimum number of insertions = " << (n - len)<< endl;
 }
 
 // Driver Code
 int main()
 {
-	string str1 = "heap";
-	string str2 = "pea";
 
+	string str1 = "";
+	string str2 = "";
+
+	cout<<"Enter string 1 : ";
+	cin>>str1;
+	cout<<"Enter string 2 : ";
+    cin>>str2;
+	cout<<"\nMinimum number of insertion/deletion required to transform \""<<str1<<"\" into \""<<str2<<"\" :\n"<<endl;
+    
 	// Function Call
 	printMinDelAndInsert(str1, str2);
 	return 0;
